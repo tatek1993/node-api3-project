06d127759310b8fa1015493827ece1a1a89e5f94
Tate Keller <tatek1993@gmail.com>
2020-03-12 14:24:54 -0500

    set up api endpoints in postRouter

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,21 +1,93 @@
 const express = require('express');
 
+const Posts = require('./postDb');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  Posts.get()
+  .then(posts => {
+    res.status(200).json(posts)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({ message: "the posts could not be retreived"})
+  })
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  Posts.getById(req.params.id)
+  .then(post => {
+    if (post === undefined) {
+        res.status(404).json({message: "The post with the specified ID does not exist."})
+    } else {
+        res.status(200).json(post)
+    }
+    
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({ 
+        error: "The post information could not be retrieved." 
+    });
+  });
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  Posts.remove(req.params.id)
+    .then(post => {
+        if (post === undefined) {
+            res.status(404).json({message: "The post with the specified ID does not exist."})
+        } else {
+            res.status(200).json(post)
+        }
+        
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ 
+            error: "The post could not be removed" 
+        });
+    });
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  const postInfo = req.body;
+
+    if (postInfo.text == false) {
+        res.status(400).json({errorMessage: "Please provide title and contents for the post"});
+        return;
+    }
+
+    Posts.update(req.params.id, postInfo)
+    .then(count => {
+        if (count === 1) {
+          
+            Posts.getById(req.params.id)
+            .then(post => {
+                if(post === undefined) 
+                {console.log("AHH", req.params.id, post);
+                    res.status(404).json({ message: "The post with the specified ID does not exist." })
+                } else {
+                    res.status(200).json(post)
+                }
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(500).json({message: "Could not update"})
+            })
+            
+        } else {
+            res.status(200).json(post)
+        }
+        
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ 
+            error: "The post information could not be retrieved." 
+        });
+    });
 });
 
 // custom middleware
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
+const postRouter = require('./posts/postRouter');
+
 const server = express();
+server.use(express.json());
+
+server.use('/api/posts', postRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);

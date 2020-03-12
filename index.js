99bf0c4a6c4cb581876b1df28027283cc15c92fc
Tate Keller <tatek1993@gmail.com>
2020-03-12 17:11:22 -0500

    deploying to heroku

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // code away!
+require('dotenv').config();
 const server = require('./server');
 
 server.listen(5000, () => {
--- server.js
+++ server.js
@@ -11,6 +11,9 @@ server.use('/api/posts', postRouter);
 server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
+  const environment = process.env;
+  const port = process.env.PORT || 5000;
+  res.status(200).json({ api: 'up', port, environment });
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
